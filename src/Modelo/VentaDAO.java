877057cff552dbbb8ddfe51f31434789fda470b1
ruthmiravalgomez <ruth.miraval.g@gmail.com>
2025-07-10 20:06:52 -0500

    Update VentaDAO.java

--- src/Modelo/VentaDAO.java
+++ src/Modelo/VentaDAO.java
@@ -4,33 +4,32 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
 
 public class VentaDAO {
+
     Connection con;
     ConexionBd cn = new ConexionBd();
     PreparedStatement ps;
     ResultSet rs;
     int r;
-    
-    public int IdVenta(){
+
+    public int IdVenta() {
         int id = 0;
         String sql = "SELECT MAX(id) FROM ventas";
         try {
             con = cn.getConnection();
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
-            if (rs.next()){
+            if (rs.next()) {
                 id = rs.getInt(1);
             }
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.toString());
         }
         return id;
     }
-    
-    public int RegistrarVenta(Venta v){
+
+    public int RegistrarVenta(Venta v) {
         String sql = "INSERT INTO ventas (cliente, vendedor, total, fecha) VALUES (?,?,?,?)";
         try {
             con = cn.getConnection();
@@ -40,19 +39,19 @@ public class VentaDAO {
             ps.setDouble(3, v.getTotal());
             ps.setString(4, v.getFecha());
             ps.execute();
-        } catch(SQLException e) {
+        } catch (SQLException e) {
             System.out.println(e.toString());
         } finally {
             try {
                 con.close();
-            } catch (SQLException e){
+            } catch (SQLException e) {
                 System.out.println(e.toString());
             }
         }
         return r;
     }
-    
-    public int RegistrarDetalle(Detalle dv){
+
+    public int RegistrarDetalle(Detalle dv) {
         String sql = "INSERT INTO detalle (cod_pro, cantidad, precio, id_venta) VALUES (?,?,?,?)";
         try {
             con = cn.getConnection();
@@ -62,51 +61,56 @@ public class VentaDAO {
             ps.setDouble(3, dv.getPrecio());
             ps.setInt(4, dv.getId());
             ps.execute();
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.toString());
         } finally {
             try {
                 con.close();
-            } catch (SQLException e){
+            } catch (SQLException e) {
                 System.out.println(e.toString());
             }
         }
         return r;
     }
-    
-    public boolean ActualizarStock (int cant, String cod){
+
+    public boolean ActualizarStock(int cant, String cod) {
         String sql = "UPDATE productos SET stock=? WHERE codigo=?";
         try {
             con = cn.getConnection();
-            ps =  con.prepareStatement(sql);
+            ps = con.prepareStatement(sql);
             ps.setInt(1, cant);
             ps.setString(2, cod);
             ps.execute();
             return true;
-        } catch (SQLException e){
+        } catch (SQLException e) {
             System.out.println(e.toString());
             return false;
         }
     }
-    
-    public List ListarVentas(){
-        List<Venta> ListaVenta = new ArrayList();
+
+    public VentaCola ListarVentas() {
+        VentaCola ventaCola = new VentaCola();
         String sql = "SELECT * FROM ventas";
+
         try {
             con = cn.getConnection();
             ps = con.prepareStatement(sql);
             rs = ps.executeQuery();
-            while(rs.next()){
+
+            while (rs.next()) {
                 Venta vent = new Venta();
                 vent.setId(rs.getInt("id"));
                 vent.setCliente(rs.getString("cliente"));
                 vent.setVendedor(rs.getString("vendedor"));
                 vent.setTotal(rs.getDouble("total"));
-                ListaVenta.add(vent);
+
+                ventaCola.agregar(vent);
             }
-        } catch(SQLException e){
-            System.out.println(e.toString());
+        } catch (SQLException e) {
+            System.out.println("Error al listar ventas: " + e.toString());
         }
-        return ListaVenta;
+
+        return ventaCola;
     }
+
 }
