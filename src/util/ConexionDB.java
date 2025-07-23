1bff51c526019c43eb739fe0678e47dff89eee78
jhonatan ramos <eramos@geosatelital.com.pe>
2025-07-23 14:54:17 -0500

    added default port for mysql

--- src/util/ConexionDB.java
+++ src/util/ConexionDB.java
@@ -8,7 +8,7 @@ public class ConexionDB {
     //library 
     private static final String DRIVER_NAME = "com.mysql.cj.jdbc.Driver";
     // database credentials
-    private static final String URL = "jdbc:mysql://localhost:3307/tiendalolitabd?serverTimezone=UTC";
+    private static final String URL = "jdbc:mysql://localhost/tiendalolitabd?serverTimezone=UTC";
     private static final String USER = "root";
     private static final String PASSWORD = "";
 
